8d2d55aa1d84618e2814298fbd60b4582458fb4e
Máximo Fain <maximofain0@gmail.com>
2023-10-07 16:58:49 -0300

    MainMenu base for first release

--- Scenes/MainMenu.gd
+++ Scenes/MainMenu.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-
+var scene_list = ["res://Scenes/WaterScene.tscn"]
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -12,5 +12,30 @@ func _process(delta):
 
 
 func _on_creditos_button_pressed():
+	$ButtonPressed.play()
 	$MainContainer.set_visible(false)
 	$CreditosContainer.set_visible(true)
+
+
+func _on_opciones_button_pressed():
+	$ButtonPressed.play()
+	$MainContainer.set_visible(false)
+	#$OpcionesContainer.set_visible(true)
+
+
+func _on_jugar_button_pressed():
+	$ButtonPressed.play()
+	await $ButtonPressed.finished
+	get_tree().change_scene_to_file(scene_list[randi() % scene_list.size()])
+
+
+func _on_jugar_button_mouse_entered():
+	$ButtonPressed.play()
+
+
+func _on_opciones_button_mouse_entered():
+	$ButtonPressed.play()
+
+
+func _on_creditos_button_mouse_entered():
+	$ButtonPressed.play()

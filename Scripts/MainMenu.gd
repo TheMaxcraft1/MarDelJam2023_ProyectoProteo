4b754f8d6e30b9533bf916e5f0961ef7f0c65239
Máximo Fain <maximofain0@gmail.com>
2023-10-08 14:08:03 -0300

    Water scene finished

--- Scripts/MainMenu.gd
+++ Scripts/MainMenu.gd
@@ -4,6 +4,7 @@ var scene_list = ["res://Scenes/WaterScene.tscn"]
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	MusicController.play_main_menu_music()
+	ScoreController.set_score(0)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -26,7 +27,7 @@ func _on_opciones_button_pressed():
 func _on_jugar_button_pressed():
 	$ButtonPressed.play()
 	await $ButtonPressed.finished
-	#get_tree().change_scene_to_file(scene_list[randi() % scene_list.size()])
+	get_tree().change_scene_to_file(scene_list[randi() % scene_list.size()])
 
 
 func _on_jugar_button_mouse_entered():

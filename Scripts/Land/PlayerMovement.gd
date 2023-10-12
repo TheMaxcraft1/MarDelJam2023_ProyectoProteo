e77c34356a45713494f549c0aacd7796c1786bd7
Máximo Fain <maximofain0@gmail.com>
2023-10-12 17:07:25 -0300

    Jump control! :3

--- Scripts/Land/PlayerMovement.gd
+++ Scripts/Land/PlayerMovement.gd
@@ -5,6 +5,7 @@ var capibaraJumpSound
 var scoreUpSound
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = 800
+var stopping_gravity = 1500
 
 func _ready():
 	capibaraJumpSound = get_parent().get_node("CapibaraJump")
@@ -12,13 +13,19 @@ func _ready():
 
 func _physics_process(delta):
 	# Add the gravity.
-	if not is_on_floor():
+	#not is_on_floor()
+	if !Input.is_action_pressed("LandJump") and velocity.y < 0:
+		velocity.y += stopping_gravity * delta
+	else:
 		velocity.y += gravity * delta
 
 	# Handle Jump.
+
 	if Input.is_action_just_pressed("LandJump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		capibaraJumpSound.play()
+		
+	print(position.y)
 
 	move_and_slide()
 

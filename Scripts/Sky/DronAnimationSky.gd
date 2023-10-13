956eee253d075362cf4ffd6657acd4096905efe5
Máximo Fain <maximofain0@gmail.com>
2023-10-13 14:34:06 -0300

    Update DronAnimationSky.gd

--- Scripts/Sky/DronAnimationSky.gd
+++ Scripts/Sky/DronAnimationSky.gd
@@ -9,6 +9,6 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	position.y = lerp(position.y, parrotNode.position.y, 5*delta)
+	position.y = lerp(position.y, parrotNode.position.y -45, 5*delta)
 	if position.y>=195:
 		position.y=195
